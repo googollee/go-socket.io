9180dfb25638dc7dbbfa8a7fb3cfe542d05d6d01
Googol Lee <i@googol.im>
2013-07-22 14:24:16 +0800

    client can call directly

--- client.go
+++ client.go
@@ -16,19 +16,14 @@ const (
 )
 
 type Client struct {
+	*Session
 	*EventEmitter
 }
 
-func NewClient() *Client {
-	return &Client{
-		EventEmitter: NewEventEmitter(),
-	}
-}
-
-func (c *Client) Run(url_, origin string) error {
+func Dial(url_, origin string) (*Client, error) {
 	u, err := url.Parse(url_)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	path := u.Path
 	if l := len(path); l > 0 && path[len(path)-1] == '/' {
@@ -47,46 +42,51 @@ func (c *Client) Run(url_, origin string) error {
 	url_ = fmt.Sprintf("%s/socket.io/%d/", u.String(), ProtocolVersion)
 	r, err := http.Get(url_)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	defer r.Body.Close()
 	if r.StatusCode != 200 {
-		fmt.Println(url_)
-		return errors.New("invalid status: " + r.Status)
+		return nil, errors.New("invalid status: " + r.Status)
 	}
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	parts := strings.SplitN(string(body), ":", 4)
 	if len(parts) != 4 {
-		return errors.New("invalid handshake: " + string(body))
+		return nil, errors.New("invalid handshake: " + string(body))
 	}
 	if !strings.Contains(parts[3], "websocket") {
-		return errors.New("server does not support websockets")
+		return nil, errors.New("server does not support websockets")
 	}
 	sessionId := parts[0]
 	wsurl := "ws" + url_[4:]
 	wsurl = fmt.Sprintf("%swebsocket/%s", wsurl, sessionId)
 	ws, err := websocket.Dial(wsurl, "", origin)
 	if err != nil {
-		fmt.Println(wsurl)
-		return err
+		return nil, err
 	}
 
 	timeout, err := strconv.ParseInt(parts[1], 10, 64)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	session := NewSession(map[string]*EventEmitter{endpoint: c.EventEmitter}, sessionId, int(timeout), false)
+	ee := NewEventEmitter()
+	session := NewSession(map[string]*EventEmitter{endpoint: ee}, sessionId, int(timeout), false)
 	transport := newWebSocket(session)
 	transport.conn = ws
 	session.transport = transport
-	fmt.Println(endpoint)
 	if endpoint != "" {
 		session.Of(endpoint).sendPacket(new(connectPacket))
 	}
-	session.loop()
-	return nil
+
+	return &Client{
+		Session:      session,
+		EventEmitter: ee,
+	}, nil
+}
+
+func (c *Client) Run() {
+	c.Session.loop()
 }
