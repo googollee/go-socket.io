449abbd7f5880ee18e63943997859cc639b07171
Googol Lee <i@googol.im>
2014-09-04 00:49:59 +0800

    add example

--- example/main.go
+++ example/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
@@ -16,6 +17,18 @@ func main() {
 		log.Println("on connection")
 		so.Join("chat")
 		so.On("chat message", func(msg string) {
+			m := make(map[string]interface{})
+			m["a"] = "你好"
+			e := so.Emit("cn1111", m)
+			//这个没有问题
+			fmt.Println("\n\n")
+
+			b := make(map[string]string)
+			b["u-a"] = "中文内容" //这个不能是中文
+			m["b-c"] = b
+			e = so.Emit("cn2222", m)
+			log.Println(e)
+
 			log.Println("emit:", so.Emit("chat message", msg))
 			so.BroadcastTo("chat", "chat message", msg)
 		})
