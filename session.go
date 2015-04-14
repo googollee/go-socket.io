15cebe5db7d9749050379e029956982f47e44766
Shea Clare <shea@fuelpowered.com>
2015-04-14 11:40:42 -0700

    Fix int/float truncation bug
    The expression 'time.Second * 1.5' is not legal,
    because the types are not compatible.
    We have to do some conversion to make it work.

--- session.go
+++ session.go
@@ -55,7 +55,7 @@ func NewSession(emitters map[string]*EventEmitter, sessionId string, timeout int
 		nameSpaces:        make(map[string]*NameSpace),
 		sendHeartBeat:     sendHeartbeat,
 		heartbeatTimeout:  time.Duration(timeout) * time.Second,
-		connectionTimeout: time.Duration(timeout) * time.Second * 1.5,
+		connectionTimeout: time.Duration(timeout) * time.Duration(float64(time.Second)*1.5),
 		Values:            make(map[interface{}]interface{}),
 		Request:           r,
 	}
