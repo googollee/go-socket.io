8b9400ee640c12f8b56504b6c2a6ecabe2c8fd6d
Googol Lee <i@googol.im>
2017-01-19 23:13:16 +0100

    add pauser for payload.Decoder

---
+++ payload/pauser.go
@@ -0,0 +1,56 @@
+package payload
+
+import "sync"
+
+type pauser struct {
+	l      sync.Mutex
+	c      *sync.Cond
+	paused bool
+	worker int
+}
+
+func newPauser() *pauser {
+	ret := &pauser{}
+	ret.c = sync.NewCond(&ret.l)
+	return ret
+}
+
+func (p *pauser) Pause() bool {
+	p.l.Lock()
+	defer p.l.Unlock()
+	if p.paused {
+		return false
+	}
+	for p.worker != 0 {
+		p.c.Wait()
+	}
+	if p.paused {
+		return false
+	}
+	p.paused = true
+	return true
+}
+
+func (p *pauser) Resume() {
+	p.l.Lock()
+	defer p.l.Unlock()
+	p.paused = false
+	p.c.Signal()
+}
+
+func (p *pauser) Working() bool {
+	p.l.Lock()
+	defer p.l.Unlock()
+	if p.paused {
+		return false
+	}
+	p.worker++
+	return true
+}
+
+func (p *pauser) Done() {
+	p.l.Lock()
+	defer p.l.Unlock()
+	p.worker--
+	p.c.Signal()
+}
