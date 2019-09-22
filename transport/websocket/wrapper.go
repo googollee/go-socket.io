0167b3d088682e186f15fe4bbd920ff3964a7426
Jonathan Chappelow <chappjc@protonmail.com>
2019-09-22 09:43:49 -0500

    make a nagger in case Close isn't called
    nagger for the writer too

--- transport/websocket/wrapper.go
+++ transport/websocket/wrapper.go
@@ -1,9 +1,11 @@
 package websocket
 
 import (
+	"fmt"
 	"io"
 	"io/ioutil"
 	"sync"
+	"time"
 
 	"github.com/googollee/go-engine.io/base"
 	"github.com/googollee/go-engine.io/transport"
@@ -32,7 +34,7 @@ func (w wrapper) NextReader() (base.FrameType, io.ReadCloser, error) {
 		w.readLocker.Unlock()
 		return 0, nil, err
 	}
-	rc := rcWrapper{w.readLocker, r}
+	rc := newRcWrapper(w.readLocker, r)
 	switch typ {
 	case websocket.TextMessage:
 		return base.FrameString, rc, nil
@@ -43,12 +45,37 @@ func (w wrapper) NextReader() (base.FrameType, io.ReadCloser, error) {
 }
 
 type rcWrapper struct {
-	l *sync.Mutex
+	nagTimer *time.Timer
+	quitNag  chan struct{}
+	l        *sync.Mutex
 	io.Reader
 }
 
+func newRcWrapper(l *sync.Mutex, r io.Reader) rcWrapper {
+	timer := time.NewTimer(30 * time.Second)
+	q := make(chan struct{})
+	go func() {
+		select {
+		case <-q:
+		case <-timer.C:
+			fmt.Println("Did you forget to Close() the ReadCloser from NextReader?")
+		}
+	}()
+	return rcWrapper{
+		nagTimer: timer,
+		quitNag:  q,
+		l:        l,
+		Reader:   r,
+	}
+}
+
 func (r rcWrapper) Close() error {
+	// Stop the nagger.
+	r.nagTimer.Stop()
+	close(r.quitNag)
+	// Attempt to drain the Reader.
 	io.Copy(ioutil.Discard, r) // reader may be closed, ignore error
+	// Unlock the wrapper's read lock for future calls to NextReader.
 	r.l.Unlock()
 	return nil
 }
@@ -72,15 +99,39 @@ func (w wrapper) NextWriter(typ base.FrameType) (io.WriteCloser, error) {
 		return nil, err
 	}
 
-	return wcWrapper{w.writeLocker, writer}, nil
+	return newWcWrapper(w.writeLocker, writer), nil
 }
 
 type wcWrapper struct {
-	l *sync.Mutex
+	nagTimer *time.Timer
+	quitNag  chan struct{}
+	l        *sync.Mutex
 	io.WriteCloser
 }
 
+func newWcWrapper(l *sync.Mutex, w io.WriteCloser) wcWrapper {
+	timer := time.NewTimer(30 * time.Second)
+	q := make(chan struct{})
+	go func() {
+		select {
+		case <-q:
+		case <-timer.C:
+			fmt.Println("Did you forget to Close() the WriteCloser from NextWriter?")
+		}
+	}()
+	return wcWrapper{
+		nagTimer:    timer,
+		quitNag:     q,
+		l:           l,
+		WriteCloser: w,
+	}
+}
+
 func (w wcWrapper) Close() error {
+	// Stop the nagger.
+	w.nagTimer.Stop()
+	close(w.quitNag)
+	// Unlock the wrapper's write lock for future calls to NextWriter.
 	defer w.l.Unlock()
 	return w.WriteCloser.Close()
 }
