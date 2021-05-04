30b754b0b9e8dbcb89bc4291c4671933e878a4e8
Saimon Shaplygin <semyuon@gmail.com>
2021-05-05 00:22:53 +0300

    rollback

--- engineio/base/packet_test.go
+++ engineio/base/packet_test.go
@@ -8,33 +8,33 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-var testPacketTypeCases = []struct {
-	b       byte
-	fType   FrameType
-	pType   PacketType
-	strByte byte
-	binByte byte
-	str     string
-}{
-	{0, FrameBinary, OPEN, '0', 0, "open"},
-	{1, FrameBinary, CLOSE, '1', 1, "close"},
-	{2, FrameBinary, PING, '2', 2, "ping"},
-	{3, FrameBinary, PONG, '3', 3, "pong"},
-	{4, FrameBinary, MESSAGE, '4', 4, "message"},
-	{5, FrameBinary, UPGRADE, '5', 5, "upgrade"},
-	{6, FrameBinary, NOOP, '6', 6, "noop"},
-
-	{'0', FrameString, OPEN, '0', 0, "open"},
-	{'1', FrameString, CLOSE, '1', 1, "close"},
-	{'2', FrameString, PING, '2', 2, "ping"},
-	{'3', FrameString, PONG, '3', 3, "pong"},
-	{'4', FrameString, MESSAGE, '4', 4, "message"},
-	{'5', FrameString, UPGRADE, '5', 5, "upgrade"},
-	{'6', FrameString, NOOP, '6', 6, "noop"},
-}
-
 func TestPacketType(t *testing.T) {
-	for i, test := range testPacketTypeCases {
+	var tests = []struct {
+		b       byte
+		fType   FrameType
+		pType   PacketType
+		strByte byte
+		binByte byte
+		str     string
+	}{
+		{0, FrameBinary, OPEN, '0', 0, "open"},
+		{1, FrameBinary, CLOSE, '1', 1, "close"},
+		{2, FrameBinary, PING, '2', 2, "ping"},
+		{3, FrameBinary, PONG, '3', 3, "pong"},
+		{4, FrameBinary, MESSAGE, '4', 4, "message"},
+		{5, FrameBinary, UPGRADE, '5', 5, "upgrade"},
+		{6, FrameBinary, NOOP, '6', 6, "noop"},
+
+		{'0', FrameString, OPEN, '0', 0, "open"},
+		{'1', FrameString, CLOSE, '1', 1, "close"},
+		{'2', FrameString, PING, '2', 2, "ping"},
+		{'3', FrameString, PONG, '3', 3, "pong"},
+		{'4', FrameString, MESSAGE, '4', 4, "message"},
+		{'5', FrameString, UPGRADE, '5', 5, "upgrade"},
+		{'6', FrameString, NOOP, '6', 6, "noop"},
+	}
+
+	for i, test := range tests {
 		typ := ByteToPacketType(test.b, test.fType)
 
 		require.Equal(t, test.pType, typ, fmt.Sprintf(`types not equal by case: %d`, i))
