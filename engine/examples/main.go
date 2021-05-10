7207e71ce8af12ea6bc53ae527f8d5b54bab0e7a
Googol Lee <i@googol.im>
2021-05-10 09:05:10 +0200

    Add comments

---
+++ engine/examples/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/googollee/go-socket.io/engine"
+)
+
+func main() {
+	eio := engine.New(engine.DefaultOption().
+		WithPingInterval(25 * time.Second).
+		WithPingTimeout(50 * time.Second))
+
+	eio.OnOpen(func(ctx engine.Context, req *http.Request) error {
+		log.Printf("engineio sid %s opened with transport %s", ctx.Session().ID(), ctx.Session().Transport())
+		ctx.Session().Store("url", req.URL.String())
+		if req.URL.Query().Get("allow") == "false" {
+			// An error means server won't continue this session and return a non-2xx response.
+			return engine.HTTPError(http.StatusNotAcceptable, "client says allow == false")
+		}
+
+		return nil
+	})
+
+	eio.OnPing(func(ctx engine.Context) {
+		log.Printf("engineio sid %s got ping", ctx.Session().ID())
+	})
+
+	eio.OnUpgrade(func(ctx engine.Context, req *http.Request) error {
+		log.Printf("engineio sid %s upgraded to transport %s", ctx.Session().ID(), ctx.Session().Transport())
+		return nil
+	})
+
+	eio.OnMessage(func(ctx engine.Context, msg io.Reader) {
+		data, err := io.ReadAll(msg)
+		if err != nil {
+			log.Fatalf("read from engineio sid %s error: %s", ctx.Session().ID(), err)
+			ctx.Session().Close()
+			return
+		}
+
+		writer, err := ctx.Session().SendFrame(engine.FrameText)
+		if err != nil {
+			log.Fatalf("next writer from engineio sid %s error: %s", ctx.Session().ID(), err)
+			ctx.Session().Close()
+			return
+		}
+		defer writer.Close()
+
+		if _, err := writer.Write(data); err != nil {
+			log.Fatalf("write to engineio sid %s error: %s", ctx.Session().ID(), err)
+			ctx.Session().Close()
+			return
+		}
+	})
+
+	eio.OnError(func(ctx engine.Context, err error) {
+		log.Printf("engineio sid %s got error: %s", ctx.Session().ID(), err)
+		ctx.Session().Close()
+	})
+
+	eio.OnClosed(func(ctx engine.Context) {
+		log.Printf("engineio sid %s closed", ctx.Session().ID())
+	})
+
+	// engine.Server implements http.Handler, which compatibles with any http frameworks.
+	// Make sure routing all methods of an endpoint to engine.Server.
+	http.Handle("/engineio", eio)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
--- engine/server.go
+++ engine/server.go
@@ -15,6 +15,7 @@ func DefaultOption() *Option {
 
 func (o *Option) WithPingInterval(time.Duration) *Option { return o }
 func (o *Option) WithPingTimeout(time.Duration) *Option  { return o }
+func (o *Option) WithMaxBufferSize(int) *Option          { return o }
 
 type FrameType int
 
@@ -29,25 +30,31 @@ func New(*Option) *Server {
 	return nil
 }
 
+// OnXXX should be called before serving HTTP.
 func (s *Server) OnOpen(func(Context, *http.Request) error)    {}
 func (s *Server) OnUpgrade(func(Context, *http.Request) error) {}
 func (s *Server) OnPing(func(Context))                         {}
 func (s *Server) OnMessage(func(Context, io.Reader))           {}
 func (s *Server) OnError(func(Context, error))                 {}
 func (s *Server) OnClosed(func(Context))                       {}
+
 func (s *Server) ServeHTTP(http.ResponseWriter, *http.Request) {}
 
+// Session methods could be called in any goroutine.
 type Session interface {
 	ID() string
-	Store(key string, value interface{})
 	Transport() string
+
 	Close() error
+	Store(key string, value interface{})
+
+	// SendFrame should be called after closed last frame.
+	SendFrame(FrameType) (io.WriteCloser, error)
 }
 
 type Context interface {
 	context.Context
 	Session() Session
-	SendFrame(FrameType) (io.WriteCloser, error)
 }
 
 func HTTPError(code int, msg string) error { return nil }
