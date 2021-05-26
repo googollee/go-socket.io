69c8a454520203fe093fa0b843613d178eac5a10
Googol Lee <i@googol.im>
2021-05-26 09:48:50 +0200

    Add comments

--- engineio/transport/factory.go
+++ engineio/transport/factory.go
@@ -7,18 +7,21 @@ import (
 
 var creators = make(map[Name]Creator)
 
+// Name is a name of transport.
 type Name string
 
 const (
 	Polling   Name = "polling"
-	Websocket      = "websocket"
-	SSE            = "sse"
+	Websocket Name = "websocket"
+	SSE       Name = "sse"
 )
 
+// Register registers a transport's creator. The creator is used when creates a transport with it's name.
 func Register(name Name, creator Creator) {
 	creators[name] = creator
 }
 
+// Create creates a transport with the name.
 func Create(name Name, pingTimeout time.Duration, callbacks Callbacks) (Transport, error) {
 	creator, ok := creators[name]
 	if !ok {
