c364bf838bec35ffd044b2ba0f92bbfe59550f6c
Semen Shaplygin <semyuon@gmail.com>
2023-02-26 17:32:39 +0300

    ref: ci (#573)

--- Makefile
+++ Makefile
@@ -8,7 +8,7 @@ get_dev:
 
 .PHONY: test
 test:
-	go clean -testcache && go test -race -cover -covermode=atomic ./...
+	go clean -testcache && go test -v -race -count=1 ./...
 
 .PHONY: bench
 bench:
--- engineio/payload/data_test.go
+++ engineio/payload/data_test.go
@@ -22,11 +22,11 @@ var tests = []struct {
 	},
 	{true, []byte{0x00, 0x01, 0x03, 0xff, '4', 'h', 'e', 'l', 'l', 'o', ' ', 0xe4, 0xbd, 0xa0, 0xe5, 0xa5, 0xbd},
 		[]Packet{
-			Packet{frame.String, packet.MESSAGE, []byte("hello 你好")},
+			{frame.String, packet.MESSAGE, []byte("hello 你好")},
 		},
 	},
 	{true, []byte{0x01, 0x01, 0x03, 0xff, 0x04, 'h', 'e', 'l', 'l', 'o', ' ', 0xe4, 0xbd, 0xa0, 0xe5, 0xa5, 0xbd}, []Packet{
-		Packet{frame.Binary, packet.MESSAGE, []byte("hello 你好")},
+		{frame.Binary, packet.MESSAGE, []byte("hello 你好")},
 	},
 	},
 	{true, []byte{
@@ -34,27 +34,27 @@ var tests = []struct {
 		0x00, 0x08, 0xff, '4', 0xe4, 0xbd, 0xa0, 0xe5, 0xa5, 0xbd, '\n',
 		0x00, 0x06, 0xff, '2', 'p', 'r', 'o', 'b', 'e',
 	}, []Packet{
-		Packet{frame.Binary, packet.MESSAGE, []byte("hello\n")},
-		Packet{frame.String, packet.MESSAGE, []byte("你好\n")},
-		Packet{frame.String, packet.PING, []byte("probe")},
+		{frame.Binary, packet.MESSAGE, []byte("hello\n")},
+		{frame.String, packet.MESSAGE, []byte("你好\n")},
+		{frame.String, packet.PING, []byte("probe")},
 	},
 	},
 	{false, []byte("1:0"), []Packet{
-		Packet{frame.String, packet.OPEN, []byte{}},
+		{frame.String, packet.OPEN, []byte{}},
 	},
 	},
 	{false, []byte("13:4hello 你好"), []Packet{
-		Packet{frame.String, packet.MESSAGE, []byte("hello 你好")},
+		{frame.String, packet.MESSAGE, []byte("hello 你好")},
 	},
 	},
 	{false, []byte("18:b4aGVsbG8g5L2g5aW9"), []Packet{
-		Packet{frame.Binary, packet.MESSAGE, []byte("hello 你好")},
+		{frame.Binary, packet.MESSAGE, []byte("hello 你好")},
 	},
 	},
 	{false, []byte("10:b4aGVsbG8K8:4你好\n6:2probe"), []Packet{
-		Packet{frame.Binary, packet.MESSAGE, []byte("hello\n")},
-		Packet{frame.String, packet.MESSAGE, []byte("你好\n")},
-		Packet{frame.String, packet.PING, []byte("probe")},
+		{frame.Binary, packet.MESSAGE, []byte("hello\n")},
+		{frame.String, packet.MESSAGE, []byte("你好\n")},
+		{frame.String, packet.PING, []byte("probe")},
 	},
 	},
 }
